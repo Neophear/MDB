6ffc66c51d21e7ecaf0bef993a859b0df822691d
Stiig Gade <stiiggade@gmail.com>
2019-10-11 07:40:43 +0200

    Removed sample project

--- MobilDataBase/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace MobilDataBase.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- MobilDataBase/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobilDataBase.Global" Language="C#" %>
